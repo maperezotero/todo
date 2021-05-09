6d64a474d03ccd14a7f8e2b2c41e2df0659503ad
maperezotero <maperezotero@gmail.com>
2021-05-09 11:13:36 +0200

    App funcionando OK - First round

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+
+	<x-slot name="header">
+		<h2 class="font-semibold text-xl text-gray-800 leading-tight">
+			{{ __('Edit Task') }}
+		</h2>
+	</x-slot>
+
+	<div class="py-12">
+		<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+			<div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+
+				<form action="/task/{{ $task->id }}" method="post">
+					@csrf
+					<div class="form-group">
+						<textarea name="description"  class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" placeholder="Enter your task">{{ $task->description }}</textarea>
+						@if ($errors->has('description'))
+							<p class="mb-2"><span class=" text-red-500 mb-4 py-1 px-2">{{ $errors->first('description') }}</span></p>
+						@endif
+					</div>
+
+					<div class="form-group">
+						<button type="submit" name="update" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update Task</button>
+					</div>
+				
+				</form>
+
+			</div>
+		</div>
+	</div>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group( function () {
+    Route::get('/dashboard', [TasksController::class, 'index'])->name('dashboard');
+
+	Route::get('/task', [TasksController::class, 'add']);
+	Route::post('/task', [TasksController::class, 'create']);
+
+	Route::get('/task/{task}', [TasksController::class, 'edit']);
+	Route::post('/task/{task}', [TasksController::class, 'update']);
+
 });
